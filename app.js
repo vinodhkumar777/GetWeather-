7f117f83c6d1c0d2e6dac43328baa5adb471dd5a
Merugu Vinodh Kumar <vinodhkumarmerugu@gmail.com>
2022-07-25 20:08:08 +0530

    changed server and html files

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express=require("express");
 const https=require("https");
+const path=require("path");
 const bodyParser= require("body-parser");
 
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
@@ -13,7 +15,9 @@ app.get("/",function(req,res){
 app.post("/",function(req,res){
    
     const city=req.body.city;
-    console.log(city)
+    if(city==""){
+        return res.send("city can not be empty")
+    }
     const query=city;
     const apiid="414e0cdd9f5992e5bffa4c1e4c54a245";
     const unit="metric";
@@ -21,15 +25,22 @@ app.post("/",function(req,res){
     https.get(url,function(response){
         //200 response code is good sign
         response.on("data",function(data){
+            //console.log(data);
             const weather=JSON.parse(data);
-            //console.log(weather);
+           // console.log(weather);
+            const code=weather.cod;
+           // console.log(typeof(code));
+            if(code==="404"){
+                return res.send("Please enter a valid city name")
+            }
+            
             const temperature=weather.main.temp;
             const weatherDesc=weather.weather[0].description;
             const icon=weather.weather[0].icon;
             let imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
             //console.log(weatherDesc);
-            res.write("<h1>The temperature in"+city+" is "+temperature+"</h1>");
-            res.write("The clouds are"+weatherDesc);
+            res.write("<h1>The temperature in "+city+" is "+temperature+"</h1>");
+            res.write("The clouds are "+weatherDesc);
            // res.write(<img src=${URL} ></img>)
             res.write(`<img src=${imageURL} ></img>`)
             res.send();
